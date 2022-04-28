0a480acfe40f11ebf34264e7595210571ea0d2c5
dongyanhuang <2779753039@qq.com>
2022-04-28 19:40:45 +0800

    third java commit

--- src/dyh/Demo1.java
+++ src/dyh/Demo1.java
@@ -3,11 +3,7 @@ package dyh;
 public class Demo1{
     public static void main(String[] args) {
         System.out.println("dyh");
-        System.out.println("dyh");
-        System.out.println("dyh");
-        System.out.println("dyh");
-        System.out.println("dyh");
-        System.out.println("dyh");
-        System.out.println("dyh");
+        System.out.println("dyh3");
+
     }
 }
